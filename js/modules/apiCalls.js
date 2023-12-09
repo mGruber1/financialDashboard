4d78c9b74727c4f369c78739023b9a74184df03e
max <marcus-gruber@gmx.at>
2023-12-09 08:54:54 +0100

    baseline for project

---
+++ js/modules/apiCalls.js
@@ -0,0 +1,76 @@
+"use strict";
+function getYearlyAverageValues() {
+  return new Promise((resolve, reject) => {
+    fetch("http://localhost:3000/api/getYearlyAverageValues")
+      .then((response) => {
+        if (!response.ok) {
+          reject("Network response was not ok");
+          return 1;
+        }
+        return response.json();
+      })
+      .then((data) => {
+        resolve(data);
+
+        return 0;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        reject(error);
+      });
+  });
+}
+function getFixedCosts() {
+  return new Promise((resolve, reject) => {
+    fetch("http://localhost:3000/api/getFixedCosts")
+      .then((response) => {
+        if (!response.ok) {
+          reject("Network response was not ok");
+          return 1;
+        }
+        return response.json();
+      })
+      .then((data) => {
+        resolve(data);
+
+        return 0;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        reject(error);
+      });
+  });
+}
+function getIncomeRate() {
+  return new Promise((resolve, reject) => {
+    fetch("http://localhost:3000/api/getIncomeRate")
+      .then((response) => {
+        if (!response.ok) {
+          reject("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        reject(error);
+      });
+  });
+}
+
+const backendURL = "http://localhost:3000/api/ConnectionTest";
+
+async function checkBackendReachability() {
+  try {
+    const response = await fetch(`${backendURL}`);
+    if (response.ok) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    return false;
+  }
+}
---
+++ server/server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+// Create a connection pool
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'datenjahr',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+app.use(cors({
+  origin: 'http://localhost',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
+
+app.get('/api/ConnectionTest', (req, res) => {
+  res.json({status: 0});
+  });
+
+app.get('/api/getAllExpenditureTypes', (req, res) => {
+  pool.query('SELECT DISTINCT type FROM expenditures', (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    res.json(results);
+  });
+});
+
+app.get('/api/getYearlyAverageValues', (req, res) => {
+  pool.query('SELECT type, AVG(amount) AS average_amount FROM expenditures WHERE year LIKE 2023 GROUP BY type ORDER BY average_amount DESC;', (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    res.json(results);
+  });
+});
+
+app.get('/api/getIncomeRate', (req, res) => {
+  pool.query('SELECT netto_pt FROM revenues;', (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    res.json(results);
+  });
+});
+
+app.get('/api/getFixedCosts', (req, res) => {
+  pool.query('SELECT rent, insurance, internet, groceries, investment_plan, car_gas,health, (rent+insurance+internet+groceries+investment_plan+car_gas+health) AS total_fixed_costs FROM fixed_costs;', (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    res.json(results);
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
