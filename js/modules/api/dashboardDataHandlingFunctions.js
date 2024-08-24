dab71464d5a399022a7c37e6b0748f31e33069b5
Marcus Gruber <marcus-gruber@gmx.at>
2024-08-24 10:23:48 +0200

    feat: made dashboard-cards

--- js/modules/api/dashboardDataHandlingFunctions.js
+++ js/modules/api/dashboardDataHandlingFunctions.js
@@ -10,6 +10,7 @@ export const handleDashboardData = (
     row.classList.add(
       "row",
       "p-2",
+      "mb-2",
       "border",
       "d-flex",
       "justify-content-center",
@@ -32,22 +33,29 @@ export const handleDashboardData = (
 
     const profits = (totalRevenues - totalExpenditures).toFixed(2);
 
-    createCard("Profits", "€ " + profits, row);
-    createCard("Revenues", "€ " + totalRevenues.toFixed(2), row);
-    createCard("Expenditures", "€ " + totalExpenditures.toFixed(2), row);
+    createCard("Profits", "€ " + profits, row, "bi-cash");
+    createCard(
+      "Revenues",
+      "€ " + totalRevenues.toFixed(2),
+      row,
+      "bi-graph-up-arrow"
+    );
+    createCard(
+      "Expenditures",
+      "€ " + totalExpenditures.toFixed(2),
+      row,
+      "bi-graph-down-arrow"
+    );
+    console.log(filteredExpenditures);
   });
 };
 
-const createCard = (title, innerHTML, row) => {
+const createCard = (title, data, row, icon) => {
   let column = document.createElement("div");
   column.classList.add("col");
 
   let card = document.createElement("div");
   card.classList.add("card");
-
-  let cardHeader = document.createElement("div");
-  cardHeader.classList.add("card-header", "bg-dark", "text-white");
-  cardHeader.innerHTML = title;
 
   let cardBody = document.createElement("div");
   cardBody.classList.add(
@@ -56,9 +64,39 @@ const createCard = (title, innerHTML, row) => {
     "justify-content-center",
     "align-items-center"
   );
-  cardBody.innerHTML = innerHTML;
 
-  card.appendChild(cardHeader);
+  let cardDataRow = document.createElement("div");
+  cardDataRow.classList.add("row", "w-100");
+
+  let cardDataCol1 = document.createElement("div");
+  cardDataCol1.classList.add("col");
+
+  let cardDataHeading = document.createElement("span");
+  cardDataHeading.classList.add("text-info");
+  cardDataHeading.innerHTML = title;
+  cardDataCol1.appendChild(cardDataHeading);
+
+  let cardData = document.createElement("h5");
+  cardData.innerHTML = data;
+  cardDataCol1.appendChild(cardData);
+
+  let cardDataCol2 = document.createElement("div");
+  cardDataCol2.classList.add(
+    "col",
+    "d-flex",
+    "justify-content-end",
+    "align-items-center"
+  );
+
+  let cardIcon = document.createElement("i");
+  cardIcon.classList.add("bi", icon, "text-secondary");
+  cardIcon.style.fontSize = "2em";
+  cardDataCol2.appendChild(cardIcon);
+
+  cardDataRow.appendChild(cardDataCol1);
+  cardDataRow.appendChild(cardDataCol2);
+
+  cardBody.appendChild(cardDataRow);
   card.appendChild(cardBody);
   column.appendChild(card);
 
